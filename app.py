8c261e9a6c51eb5e4665d60175696e5e5ebd34f0
Szupee <66744652+Szupee@users.noreply.github.com>
2025-10-07 20:08:21 +0100

    working ish

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    app_name = os.getenv('APP_NAME', 'My Flask App')  # Default if not set
+    return render_template('index.html', app_name=app_name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
